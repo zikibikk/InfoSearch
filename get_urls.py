e25384e2adfcad907a5e26846df5660a8f26b713
Alina <zikibikk@gmail.com>
2024-02-19 14:26:58 +0300

    Initial commit

---
+++ get_urls.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+def get_links(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a', href=True)
+        absolute_links = [urljoin(response.url, link['href']) for link in links]
+        return absolute_links
+    else:
+        return []
+
+
+def crawl_site(start_url, max_links=100):
+    visited_urls = set()
+    links_to_visit = [start_url]
+    crawled_links = []
+
+    while links_to_visit:
+        current_url = links_to_visit.pop(0)
+        if current_url not in visited_urls:
+            visited_urls.add(current_url)
+            print(f"Crawling {current_url}")
+
+            links_on_page = get_links(current_url)
+            for link in links_on_page:
+                if len(crawled_links) >= max_links:
+                    return crawled_links
+                if link not in visited_urls:
+                    links_to_visit.append(link)
+                    crawled_links.append(link)
+
+    return crawled_links
+
+
+def save_links_to_file(links, filename):
+    with open(filename, 'w') as file:
+        for link in links:
+            file.write(link + '\n')
+
+
+start_url = 'https://ru.wikipedia.org/wiki/Древнегреческая_мифология'
+crawled_links = crawl_site(start_url, max_links=200)
+
+output_file = 'crawled_links.txt'
+save_links_to_file(crawled_links, output_file)
+
---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from requests.exceptions import RequestException
+
+
+def download_html_pages(url_list, output_dir):
+    html_contents = {}
+    for i, url in enumerate(url_list):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                html_contents[url] = response.text
+                filename = os.path.join(output_dir, f"{str(i+1)}.html")
+                with open(filename, "w", encoding="utf-8") as file:
+                    file.write(response.text)
+            else:
+                print(f"Ошибка загрузки {url}: Статус код {response.status_code}")
+        except RequestException as e:
+            print(f"Ошибка загрузки {url}: {e}")
+    return html_contents
+
+
+def download_and_save_files(filepath, output_dir):
+    urls = get_links_from_file(filepath)
+    html_pages = download_html_pages(urls, output_dir)
+    with open("index.txt", "w", encoding="utf-8") as file:
+        for i, url in enumerate(html_pages.keys(), start=1):
+            file.write(f"{i} - {url}\n")
+            # Можно добавить запись содержимого страницы в файл, если это необходимо
+    print(f"Скачивание завершено. Записано {len(html_pages)} постов в index.txt.")
+
+
+def get_links_from_file(filename):
+    links = []
+    with open(filename, 'r') as file:
+        for line in file:
+            links.append(line.strip())
+
+    return links
+
+
+output_directory = "files"
+os.makedirs(output_directory, exist_ok=True)
+download_and_save_files('crawled_links.txt', output_directory)
